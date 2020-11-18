7c7e19f5166bcdfa8c811dd7cf1919f1e0e8d7d1
kvminski <48525441+kvminski@users.noreply.github.com>
2020-11-18 13:18:09 +0100

    coding challenge 3 done
    and all lectures up to that

--- new-course/01-Fundamentals-Part-1/starter/assignment.js
+++ new-course/01-Fundamentals-Part-1/starter/assignment.js
@@ -50,7 +50,36 @@ console.log('19' - '13' + '17'); // 617
 console.log('19' - '13' + 17); // 23
 console.log('123' < 57); // false
 console.log(5 + 6 + '4' + 9 - 4 - 2); // 1143
+
+
+// Equality operators assignments
+
+const numNeighbours = Number(prompt('How many neighbour countries does your country have?'));
+
+if (numNeighbours === 1) {
+    console.log('Only 1 border!');
+} else if (numNeighbours > 1) {
+    console.log('More than 1 border!');
+} else {
+    console.log('No borders');
+}
+
+
+// Logical operators
+
+const language2 = 'english';
+const population2 = 49;
+const isIsland2 = false;
+const country = 'Poland';
+
+if (language2 === 'english' && population2 < 50 && !isIsland2) {
+    console.log(`You should live in ${country}!`);
+} else {
+    console.log(`${country} does not meet you criteria :(`);
+}
+
 */
+
 /*
 //////////////////////////////
 // Coding challenge #1
@@ -73,6 +102,7 @@ const markHigherBMI = bmiMark > bmiJohn;
 console.log(markMass, markHeight, bmiMark);
 console.log(johnMass, johnHeight, bmiJohn);
 console.log(markHigherBMI);
+
 //////////////////////////////
 // Coding challenge #2
 
@@ -86,5 +116,58 @@ if (bmiMark > bmiJohn) {
     console.log(`Mark's BMI (${bmiMark}) is higher than John's (${bmiJohn})!`);
 } else {
     console.log(`John's BMI (${bmiJohn}) is higher than Mark's (${bmiMark})!`)
+}
+
+
+//////////////////////////////
+// Coding challenge #3
+
+// task 1
+console.log("// Task 1");
+
+const teamDolphins = (96 + 108 + 89) / 3;
+const teamKoalas = (88 + 91 + 110) / 3;
+console.log(teamDolphins, teamKoalas);
+
+if (teamDolphins > teamKoalas) {
+    console.log("Team Dolphins has higher score and is the winner!");
+} else if (teamDolphins < teamKoalas) {
+    console.log("Team Koalas has higher score and is the winner!");
+} else {
+    console.log("It is a draw!");
+}
+
+console.log("// Bonus 1");
+
+const teamDolphinsBonus1 = (97 + 112 + 101) / 3;
+const teamKoalasBonus1 = (109 + 95 + 123) / 3;
+console.log(teamDolphinsBonus1, teamKoalasBonus1);
+
+if (teamDolphinsBonus1 > teamKoalasBonus1 && teamDolphinsBonus1 > 100) {
+    console.log("Team Dolphins has higher score, it is higher than 100 so is the winner!");
+} else if (teamKoalasBonus1 > teamDolphinsBonus1 && teamKoalasBonus1 >= 100) {
+    console.log("Team Koalas has higher score, it is higher than 100 so is the winner!");
+} else if (teamDolphinsBonus1 === teamKoalasBonus1) {
+    console.log("It is a draw!")
+} else {
+    console.log("It seems that no any team has score higher than 100!")
+}
+
+console.log("// Bonus 2");
+
+const teamDolphinsBonus2 = (97 + 112 + 101) / 3;
+const teamKoalasBonus2 = (109 + 95 + 106) / 3;
+console.log(teamDolphinsBonus2, teamKoalasBonus2);
+
+if (teamDolphinsBonus2 > teamKoalasBonus2 && teamDolphinsBonus2 > 100) {
+    console.log("Team Dolphins has higher score, it is higher than 100 so is the winner!");
+} else if (teamKoalasBonus2 > teamDolphinsBonus2 && teamKoalasBonus2 >= 100) {
+    console.log("Team Koalas has higher score, it is higher than 100 so is the winner!");
+} else if (teamDolphinsBonus2 === teamKoalasBonus2 && teamKoalasBonus2 >= 100 && teamDolphinsBonus2 >= 100) {
+    console.log("It is a draw! And both teams have score higher than 100!")
+} else if (teamDolphinsBonus2 === teamKoalasBonus2 && teamKoalasBonus2 <= 100 && teamDolphinsBonus2 <= 100) {
+    console.log("It is a draw, but both teams have score lower than 100!");
+} else {
+    console.log("It seems that no any team has score higher than 100!")
 }
 */
--- new-course/01-Fundamentals-Part-1/starter/script.js
+++ new-course/01-Fundamentals-Part-1/starter/script.js
@@ -181,7 +181,89 @@ let n = '1' + 1; // 11
 console.log(n);
 n = n - 1; // 10
 console.log(n);
-*/
+
 
 ////////////////////////////////////
-// 
+// truthy and falsy values
+
+// 5 falsy values: 0, '', undefined, null, NaN
+
+console.log(Boolean(0));
+console.log(Boolean(undefined));
+console.log(Boolean('Jonas'));
+console.log(Boolean(''));
+
+const money = 0;
+
+if (money) {
+    console.log("Don't spend it all");
+} else {
+    console.log("You should get a job");
+}
+
+let height;
+
+if (height) {
+    console.log("Yay! Height is defined");
+} else {
+    console.log("Height is UNDEFINED");
+}
+
+
+////////////////////////////////////
+// equality operators
+
+const age = 18;
+
+if (age === 18) console.log('You just became an adult (strict)');
+if (age == 18) console.log('You just became an adult (loose)');
+
+const favorite = Number(prompt("What is your favorite number?"));
+
+console.log(favorite);
+console.log(typeof favorite);
+
+if (favorite === 23) { // 23 === 23
+    console.log('Cool! 23 is an amazing number!');
+} else if (favorite === 7) {
+    console.log('7 is also a cool number');
+} else if (favorite === 9) {
+    console.log('9 is also a cool number');
+} else {
+    console.log('Number is not 23, 7 or 9');
+}
+
+if (favorite !== 23) {
+    console.log("why not 23?")
+}
+
+
+
+////////////////////////////////////
+// logical operators
+
+const hasDriverLicense = true; // A
+const hasGoodVision = true; // B
+
+console.log(hasDriverLicense && hasGoodVision);
+console.log(hasDriverLicense || hasGoodVision);
+console.log(!hasDriverLicense);
+
+//const shouldDrive = hasDriverLicense && hasGoodVision;
+
+// if (hasDriverLicense && hasGoodVision) {
+//     console.log("Sarah is able to drive");
+// } else {
+//     console.log("Someone else should drive");
+// }
+
+const isTired = false; // C
+console.log(hasDriverLicense || hasGoodVision || isTired);
+
+if (hasDriverLicense && hasGoodVision && !isTired) {
+    console.log("Sarah is able to drive");
+} else {
+    console.log("Someone else should drive");
+}
+
+*/
