873328cd8a0f64b283bbed7211567ee6494bc7c4
kvminski <48525441+kvminski@users.noreply.github.com>
2020-11-21 14:46:34 +0100

    fundamentals-part-1 done

--- new-course/01-Fundamentals-Part-1/starter/assignment.js
+++ new-course/01-Fundamentals-Part-1/starter/assignment.js
@@ -170,4 +170,35 @@ if (teamDolphinsBonus2 > teamKoalasBonus2 && teamDolphinsBonus2 > 100) {
 } else {
     console.log("It seems that no any team has score higher than 100!")
 }
+
+
+//////////////////////////////
+// Coding challenge #4
+
+let bill;
+let tip;
+
+bill = 275;
+tip = bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+console.log(tip);
+console.log(`The bill was ${bill}, the tip was ${tip}, and the total value was ${bill + tip}`);
+
+bill = 40;
+tip = bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+console.log(`The bill was ${bill}, the tip was ${tip}, and the total value was ${bill + tip}`);
+
+bill = 430;
+tip = bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+console.log(`The bill was ${bill}, the tip was ${tip}, and the total value was ${bill + tip}`);
+
+
+// bill = 275;
+// let tip;
+// if (50 <= bill <= 300) {
+//     tip = bill * 0.15;
+// } else {
+//     tip = bill * 0.2;
+// }
+// console.log(bill, tip, bill + tip);
+
 */
--- new-course/01-Fundamentals-Part-1/starter/script.js
+++ new-course/01-Fundamentals-Part-1/starter/script.js
@@ -266,4 +266,68 @@ if (hasDriverLicense && hasGoodVision && !isTired) {
     console.log("Someone else should drive");
 }
 
+////////////////////////////////////
+// switch statement
+
+const day = 'sunday';
+
+switch (day) {
+    case 'monday':
+        console.log("Plan course structure");
+        console.log("Go to coding meetup");
+        break;
+    case 'tuesday':
+        console.log("Prepare theory videos");
+        break;
+    case 'wednesday':
+    case 'thursday':
+        console.log("Write code examples");
+        break;
+    case 'friday':
+        console.log("Record videos");
+        break;
+    case 'saturday':
+    case 'sunday':
+        console.log("Enjoy the weekend");
+        break;
+    default:
+        console.log("Not a valid day");
+}
+
+if (day === 'monday') {
+    console.log("Plan course structure");
+    console.log("Go to coding meetup");
+} else if (day === 'tuesday') {
+    console.log("Prepare theory videos");
+} else if (day === 'wednesday' || day === 'thursday') {
+    console.log("Write code examples");
+} else if (day === 'friday') {
+    console.log("Record videos");
+} else if (day === 'saturday' || day === 'sunday') {
+    console.log("Enjoy the weekend");
+} else {
+    console.log('Not a valid day');
+}
+
+////////////////////////////////////
+// conditional tenary operator
+
+const age = 23;
+
+age >= 18 ? console.log('I like to drink wine') : console.log('I like to drink water');
+
+const drink = age >= 18 ? 'wine' : 'water';
+console.log(drink);
+
+let drink2;
+
+if (age >= 18) {
+    drink2 = "wine";
+} else {
+    drink2 = 'water';
+}
+console.log(drink2);
+
+console.log(`I like to drink ${age >= 18 ? 'wine' : 'water'}`);
+
 */
