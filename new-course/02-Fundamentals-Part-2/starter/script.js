bb5c83ff8328992e9a07326ee9d97ed549069c17
kvminski <48525441+kvminski@users.noreply.github.com>
2020-11-21 15:13:18 +0100

    fundamentals part 2 start

---
+++ new-course/02-Fundamentals-Part-2/starter/script.js
@@ -0,0 +1,15 @@
+'use strict';
+
+/////////////////////////
+// strict mode
+/*
+let hasDriversLicense = false;
+const passTest = true;
+
+if (passTest) hasDriversLicense = true;
+
+if (hasDriversLicense) console.log('I can drive');
+
+// const interface = 'Audio';
+// const private = 423;
+*/
