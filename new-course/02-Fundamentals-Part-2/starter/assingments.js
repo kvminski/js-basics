df0240de027b2fbf0c3b44e7da56719ef5ece9c5
kvminski <48525441+kvminski@users.noreply.github.com>
2021-02-27 17:33:11 +0100

    training resumes!

---
+++ new-course/02-Fundamentals-Part-2/starter/assingments.js
@@ -0,0 +1,111 @@
+/////////////////////////
+// functions
+
+// function describeCountry(country, population, capitalCity) {
+//     const countryLog = `${country} has ${population} milion people and its capital city is ${capitalCity}.`;
+//     return countryLog;
+// }
+
+// const countryPoland = describeCountry('Poland', 38, 'Warszawa');
+// console.log(countryPoland);
+// const countryFinland = describeCountry('Finland', 6, 'Helsinki');
+// console.log(countryFinland);
+// const countryGermany = describeCountry('Germany', 83, 'Berlin');
+// console.log(countryGermany);
+/*
+//better one
+
+function describeCountry(country, population, capitalCity) {
+    return `${country} has ${population} milion people and its capital city is ${capitalCity}.`;
+}
+
+const descPoland = describeCountry('Poland', 38, 'Warszawa');
+const descFinland = describeCountry('Finland', 6, 'Helsinki');
+const descGermany = describeCountry('Germany', 83, 'Berlin');
+console.log(descPoland, descFinland, descGermany);
+
+//function declarations vs expressions
+
+function percentageOfWorld1(population) {
+    //const popPercentage = (population / 7900) * 100;
+    //return popPercentage;
+    return (population / 7900) * 100;
+}
+
+const popPercentageChina = percentageOfWorld1(1393);
+const popPercentagePoland = percentageOfWorld1(38);
+const popPercentageUSA = percentageOfWorld1(328);
+
+console.log(popPercentageChina, popPercentagePoland, popPercentageUSA);
+
+const percentageOfWorld2 = function (population) {
+    return (population / 7900) * 100;
+}
+
+const popChina = percentageOfWorld2(1393);
+const popPoland = percentageOfWorld2(38);
+const popUSA = percentageOfWorld2(328);
+
+console.log(popChina, popPoland, popUSA);
+
+//arrow functions
+
+const percentageOfWorld3 = population => (population / 7900) * 100;
+
+const popChina2 = percentageOfWorld3(1393);
+const popPoland2 = percentageOfWorld3(38);
+const popUSA2 = percentageOfWorld3(328);
+
+console.log(popChina2, popPoland2, popUSA2);
+
+//Functions calling other functions
+
+function percentageOfWorld1(population) {
+    const perMathRound = (population / 7900) * 100;
+
+    return Math.round((perMathRound + Number.EPSILON) * 100) / 100
+}
+
+const describePopulation = function (country, population) {
+    const percentage = percentageOfWorld1(population);
+    const description = `${country} has ${population} milion people, which is about ${percentage}% of the world.`;
+    console.log(description);
+}
+
+describePopulation('China', 1393);
+describePopulation('Poland', 38);
+describePopulation('USA', 328);
+
+
+
+////////////////////////
+// CODING CHALLENGE #1
+////////////////////////
+
+const calcAverage = (a, b, c) => (a + b + c) / 3;
+
+//test 1
+const scoreDolphins2 = calcAverage(44, 23, 71);
+const scoreKoalas2 = calcAverage(65, 54, 49);
+
+//test 2
+const scoreDolphins = calcAverage(85, 54, 41);
+const scoreKoalas = calcAverage(23, 34, 27);
+
+console.log(scoreDolphins, scoreKoalas);
+
+
+const checkWinner = function (avgDolphins, avgKoalas) {
+    if (avgDolphins >= 2 * avgKoalas) {
+        console.log(`Dolphins win! (${avgDolphins} vs. ${avgKoalas})`);
+    } else if (avgKoalas >= 2 * avgDolphins) {
+        console.log(`Koalas win! (${avgKoalas} vs. ${avgDolphins})`);
+    } else {
+        console.log(`Dolphis got ${avgDolphins}, Koalas got ${avgKoalas}. Nobody wins!`);
+    }
+}
+
+checkWinner(scoreDolphins, scoreKoalas);
+checkWinner(scoreDolphins2, scoreKoalas2);
+
+*/
--- new-course/02-Fundamentals-Part-2/starter/script.js
+++ new-course/02-Fundamentals-Part-2/starter/script.js
@@ -2,6 +2,7 @@
 
 /////////////////////////
 // strict mode
+
 /*
 let hasDriversLicense = false;
 const passTest = true;
@@ -12,4 +13,104 @@ if (hasDriversLicense) console.log('I can drive');
 
 // const interface = 'Audio';
 // const private = 423;
+
+
+/////////////////////////
+// functions
+
+function logger() {
+    console.log("My name is Jonas");
+}
+
+//calling / running function
+logger();
+
+function fruitProcessor(apples, oranges) {
+    const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+    return juice;
+}
+
+const appleJuice = fruitProcessor(5, 0);
+console.log(appleJuice);
+
+const appleOrangeJuice = fruitProcessor(2, 4);
+console.log(appleOrangeJuice);
+
+
+/////////////////////////
+// functions
+
+// function declaration
+function calcAge1(birthYear) {
+    return 2037 - birthYear;
+}
+
+const age1 = calcAge1(1991);
+console.log(age1);
+
+//function expression
+const calcAge2 = function (birthYear) {
+    return 2037 - birthYear;
+}
+
+const age2 = calcAge2(1991);
+console.log(age1, age2);
+
+
+
+//arrow function
+const calcAge3 = birthYear => 2037 - birthYear;
+
+const age3 = calcAge3(1991);
+console.log(age3);
+
+const yearsUntilRetirement = (birthYear, firstName) => {
+    const age = 2037 - birthYear;
+    const retirement = 65 - age;
+    //return retirement;
+    return `${firstName} retires in ${retirement} years`;
+}
+
+console.log(yearsUntilRetirement(1991, 'Jonas'));
+console.log(yearsUntilRetirement(1980, 'Bob'));
+
+
+
+function fruitCutter(fruit) {
+    return fruit * 4;
+}
+
+function fruitProcessor(apples, oranges) {
+    const applePieces = fruitCutter(apples);
+    const orangePieces = fruitCutter(oranges);
+
+    const juice = `Juice with ${applePieces} pieces of apples and ${orangePieces} pieces of oranges.`;
+    return juice;
+}
+
+console.log(fruitProcessor(2, 3));
+
+
+
+const calcAge = function (birthYear) {
+    return 2037 - birthYear;
+}
+
+const yearsUntilRetirement = function (birthYear, firstName) {
+    const age = calcAge(birthYear);
+    const retirement = 65 - age;
+
+    if (retirement > 0) {
+        console.log(`${firstName} retires in ${retirement} years`);
+        return retirement;
+    } else {
+        console.log(`${firstName} has already retired`);
+        return -1;
+    }
+
+}
+
+console.log(yearsUntilRetirement(1991, 'Jonas'));
+console.log(yearsUntilRetirement(1950, 'Mike'));
+
 */
